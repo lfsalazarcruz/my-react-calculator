83a6f709809745bfe4d406a1d767ca0d482e89eb
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-12-26 14:11:18 -0800

    Calculator buttons and some styling. No functionality.

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -2,10 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      current: 0,
+    }
+  }
   render() {
     return (
       <div className="App">
-        This is app
+        <div className="calculator-output">{this.state.current}</div>
+        <div className="calculator-row">
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>x</button>
+        </div>
+        <div className="calculator-row">
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+        </div>
+        <div className="calculator-row">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+        </div>
+        {/* <button>÷</button> */}
+        <div className="calculator-row">
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
       </div>
     );
   }
