8789123bdec92ffeb07879c034f1205182090153
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-12-26 14:40:48 -0800

    Finished styling calculator.

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -11,30 +11,37 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="calculator-output">{this.state.current}</div>
-        <div className="calculator-row">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>x</button>
+        <div className="calculator-output">
+          <p className="output-text">{this.state.current}</p>
         </div>
         <div className="calculator-row">
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
+          <button className="custom-button">C</button>
+          <button className="custom-button">+/-</button>
+          <button className="custom-button">%</button>
+          <button className="custom-button">÷</button>
         </div>
         <div className="calculator-row">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
+          <button className="custom-button">7</button>
+          <button className="custom-button">8</button>
+          <button className="custom-button">9</button>
+          <button className="custom-button">x</button>
         </div>
-        {/* <button>÷</button> */}
         <div className="calculator-row">
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          <button className="custom-button">4</button>
+          <button className="custom-button">5</button>
+          <button className="custom-button">6</button>
+          <button className="custom-button">-</button>
+        </div>
+        <div className="calculator-row">
+          <button className="custom-button">1</button>
+          <button className="custom-button">2</button>
+          <button className="custom-button">3</button>
+          <button className="custom-button">+</button>
+        </div>
+        <div className="calculator-row">
+          <button className="custom-button-2">0</button>
+          <button className="custom-button">.</button>
+          <button className="custom-button">=</button>
         </div>
       </div>
     );
